23c423c592321f5cfc509bb8a1713fefcf967f52
gimbeomchang <fnzl89@gmail.com>
2024-11-21 09:22:13 +0900

    [config] husky, lint-staged

---
+++ lint-staged.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  "**/*.{js,jsx,ts,tsx}": [
+    () => "tsc --noEmit",
+    "eslint --fix",
+    "prettier --write",
+  ],
+  "**/*.{json,md,yml,yaml,css,scss}": ["prettier --write"],
+};
