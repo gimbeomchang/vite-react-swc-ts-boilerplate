dd0956437b92e324864c73cadd245110698434ad
gimbeomchang <fnzl89@gmail.com>
2024-11-21 09:57:30 +0900

    [config] prettier

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,8 +1,4 @@
 module.exports = {
-  "**/*.{js,jsx,ts,tsx}": [
-    () => "tsc --noEmit",
-    "eslint --fix",
-    "prettier --write",
-  ],
-  "**/*.{json,md,yml,yaml,css,scss}": ["prettier --write"],
+  '**/*.{js,jsx,ts,tsx}': [() => 'tsc --noEmit', 'eslint --fix', 'prettier --write'],
+  '**/*.{json,md,yml,yaml,css,scss}': ['prettier --write'],
 };
