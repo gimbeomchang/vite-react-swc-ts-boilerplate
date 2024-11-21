9abf0b57a1b8dba7c8bd7644fd922c87d02ff0c4
gimbeomchang <fnzl89@gmail.com>
2024-11-21 11:11:31 +0900

    [config] locator.js

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
+import setupLocatorUI from '@locator/runtime';
 import { createRoot } from 'react-dom/client';
 
 import { StrictMode } from 'react';
 
 import App from './App.tsx';
 import './index.css';
+
+if (import.meta.env.DEV) setupLocatorUI();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
